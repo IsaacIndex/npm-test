2f0cea3c2b61e8cc4c91858db9ecb3b6adbe87d9
IsaacIndex <isaacwongnh@gmail.com>
2024-07-26 15:11:30 +0900

    update to include dist

---
+++ dist/npm-test.js
@@ -0,0 +1,5 @@
+import { jsx as n } from "react/jsx-runtime";
+const r = () => /* @__PURE__ */ n("div", { children: /* @__PURE__ */ n("h1", { children: "My First Component" }) });
+export {
+  r as default
+};
